3e57f782d16b3afdc180f7cf169ac558e1ac39b3
Habibata Samake <habibata.samake@grenoble-inp.org>
2024-10-05 13:54:13 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def main():
+    st.title("Hello, World!")
+    st.write("Ceci est une application Streamlit déployée via CI/CD avec GitHub Actions.")
+
+if __name__ == "__main__":
+    main()
